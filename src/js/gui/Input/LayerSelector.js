b6d1e143892e1a6769070e2bd7699419dfd98d23
Max Kistner <max.kistner@uni-heidelberg.de>
2026-06-09 08:19:38 +0200

    Directly push 'this' to LayerSelector.objects
    Instead of creating a variable beforehand. It seems like 'self' was
    not used anywhere else.

--- src/js/gui/Input/LayerSelector.js
+++ src/js/gui/Input/LayerSelector.js
@@ -80,9 +80,7 @@ export class LayerSelector extends Input {
             ...options
         })
 
-        self = this; // here
-
-        LayerSelector.objects.push(self);
+        LayerSelector.objects.push(this);
     }
 };
 
